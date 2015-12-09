69b8294008b24250612a65c2620a6fabe7b2c59c
Jean Tessier <jean@jeantessier.com>
2015-12-09 11:48:33 -0800

    Setup rabbitmq-native plugin.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -118,4 +118,14 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+
+   info    'grails.app'
+}
+
+rabbitmq {
+
+    connection = {
+        connection host: "localhost", username: "guest", password: "guest"
+    }
+
 }
